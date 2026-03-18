129db6e2bab1ae16e89d262b6a037f163e7725ab
Tom Hamblin <TomHamblin@wsftb.local>
2026-03-18 14:56:09 +0000

    new test

--- main.py
+++ main.py
@@ -53,22 +53,5 @@ def main():
         print(f"Episode {start} scraped")
 
 
-# def main():
-#     for i in range(20, 81):
-#         print("Working...")
-#         payload = turn_to_string(split_transcript(grab_transcript(fetch_episode(i))))
-
-#         prompt = PROMPT_TEMPLATE.replace("{transcript_text}", payload).replace(
-#             "{episode_number}", f"{i}"
-#         )
-
-#         raw_output = decipher_questions(prompt)
-
-#         with open(f"episode_output/{i}.json", "w", encoding="utf-8") as f:
-#             f.write(raw_output)
-
-#         print(f"Episode {i} scraped.")
-
-
 # if __name__ == "__main__":
 #     main()
