e92f42b452c176a67d7f3c359b8867ddc8ec63d6
Tom Hamblin <tom@home.darkwoods>
2026-03-19 00:03:48 +0000

    Added log file + logging

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fetcher import fetch_episode
 from parser import grab_transcript, split_transcript, turn_to_string
 from gpt import decipher_questions
 from prompt import PROMPT_TEMPLATE
+import datetime as dt
 import os
 
+def get_time():
+    return dt.now().strftime("%Y-%m-%d %H:%M:%S")
 
 def starting_episode():
     starting_episode = 1
@@ -32,9 +35,11 @@ start = starting_episode()
 
 def main():
     loop_start = start
+    log_file = open("log.txt", "a", encoding="utf-8")
 
     while True:
         print("Working...")
+        log_file.write(f"[{get_time()}] Working...")
 
         ep_ts = grab_transcript(fetch_episode(loop_start))
 
@@ -53,6 +58,8 @@ def main():
             f.write(raw_output)
 
         print(f"Episode {loop_start} scraped")
+        log_file.write(f"[{get_time()}] Episode {loop_start} scraped.")
+        log_file.close()
 
         loop_start += 1
 
